a8111cfd75dbc5c9fa7fb37460b8e8b5e1b73b0a
Scott Milliorn <scottmilliorn@gmail.com>
2023-08-09 00:57:54 -0700

    Update astro.config.mjs
    Signed-off-by: Scott Milliorn <scottmilliorn@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  sitemap: true,
-  site: 'https://gitlab.com/milliorn/portfolio',
-  outDir: 'public',
-});
+export default defineConfig({});
