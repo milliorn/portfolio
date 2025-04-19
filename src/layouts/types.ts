f03b4cfd783de48a40e0b39e386f7accc5f8c481
milliorn <scottmilliorn@gmail.com>
2025-04-19 15:30:57 -0700

    moved types into file in layout

---
+++ src/layouts/types.ts
@@ -0,0 +1,11 @@
+import type { MarkdownLayoutProps } from "astro";
+
+export type BlogProps = MarkdownLayoutProps<{
+  poster: string;
+  subtitle: string;
+  title: string;
+}>;
+
+export type MainProps = {
+  title: string
+}
