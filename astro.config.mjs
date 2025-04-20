e010c930cf552c4864165444f5a1e12e6c85fa72
milliorn <scottmilliorn@gmail.com>
2025-04-19 17:54:36 -0700

    integrate site index i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://milliorn-portfolio.netlify.app/", // ← helpful for meta helpers, RSS, SEO
   output: "server",
   adapter: netlify(),
+  integrations: [
+    sitemap({
+      i18n: {
+        // https://docs.astro.build/en/guides/integrations-guide/sitemap/#i18n
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://website.com/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+        },
+      },
+    }),
+  ],
 });
