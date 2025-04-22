04afbf32276bbf70ef771cbd7675cfc687237034
milliorn <scottmilliorn@gmail.com>
2025-04-22 15:56:09 -0700

    reorganize props

--- src/props/Layouts.types.ts
+++
@@ -1,8 +0,0 @@
-import type { MarkdownLayoutProps } from "astro";
-import type { BlogFrontMatter } from "./Pages.types";
-
-export type BlogProps = MarkdownLayoutProps<BlogFrontMatter>;
-
-export type MainProps = {
-  title: string;
-};
