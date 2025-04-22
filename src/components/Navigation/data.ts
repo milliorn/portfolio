8450a4ad5a486c658d85c3f27969d241107f2ca0
milliorn <scottmilliorn@gmail.com>
2025-04-22 14:43:16 -0700

    fixed imports, refactor links and navigation

---
+++ src/components/Navigation/data.ts
@@ -0,0 +1,29 @@
+import type { NavLink } from "./NavLink";
+
+export const navLinks: NavLink[] = [
+  {
+    href: "/",
+    label: "Home",
+    ariaLabel: "Go to Home page",
+  },
+  {
+    href: "/about",
+    label: "About",
+    ariaLabel: "Learn more about me",
+  },
+  {
+    href: "/work",
+    label: "Work",
+    ariaLabel: "View work history and professional experience",
+  },
+  {
+    href: "/tech",
+    label: "Tech",
+    ariaLabel: "See what technologies I enjoy",
+  },
+  {
+    href: "/philosophies",
+    label: "Philosophies",
+    ariaLabel: "Explore my guiding principles",
+  },
+];
